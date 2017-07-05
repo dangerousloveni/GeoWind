b8b53fbb8afeedb4c9a7da917f7a5dbe4e44270c
dangerousloveni <1281541085@qq.com>
2017-07-05 16:12:37 +0800

    Update MainActivity.java

--- app/src/main/java/xx/com/geowind/MainActivity.java
+++ app/src/main/java/xx/com/geowind/MainActivity.java
@@ -9,13 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int c;
-        int p;
-        int a;
-        int d;
-        int aa;
-        int f;
-        int s;
+        int C;
+
 
 
     }
